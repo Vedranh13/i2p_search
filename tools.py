d170e4a722bce0878a112d6e50623a6bf934fafa
Vedran Hadziosmanovic <vedranh13@gmail.com>
2016-07-16 15:53:47 -0500

    Added a method to generate blank .search files

--- tools.py
+++ tools.py
@@ -10,13 +10,26 @@ def removeDupsFromFile ( path ):
             file.write ( line )
             writen_lines.append ( line )
 #TODO Download more RAM?
+def generateCleanDotSearches ():
+    myFile = open ( "./up_eepsites.search" , "w" )
+    myFile.close()
+    myFile2 = open ( "./down_eepsites.search" , "w" )
+    myFile2.close()
+    myFile3 = open ( "./somethingWeirdHappened.search" , "w" )
+    myFile3.close()
+    myFile4 = open ( "./known_eepsites.search" , "w" )
+    myFile4.close()
+    myFile5 = open ( "./non_eepsites.search" , "w" )
+    myFile5.close()
 def main( argv ):
     try:
-      opts, args = getopt.getopt( argv , "hD:" )
+      opts, args = getopt.getopt( argv , "hD:G" )
     except getopt.GetoptError:
-       print ( "tools.py -hd:D:" )
+       print ( "tools.py -hD:G" )
        exit(1)
     for flag, arg in opts:
-        if ( flag in ( "-D" , "" ) or flag in ( "-d" , "" ) ):
+        if flag in ( "-D" , "" ):
             removeDupsFromFile ( arg ) #TODO Should probably check this is a valid path
+        if flag in ( "-G" , "" ):
+            generateCleanDotSearches()
 main ( sys.argv[1:] )
